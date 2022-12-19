5b23bd7c4f44f2b9072b93446ca43923275f369f
VH <hirawatt@pm.me>
2022-12-19 17:42:18 +0530

    MVP - Working

---
+++ drive_image.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import requests
+
+image_url = "https://drive.google.com/open?id=1a_MTMISVup-7c22tpmnrRolp4z8TP7RO"
+
+
+r = requests.get(image_url)
--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import streamlit as st
 import pandas as pd
 
+# Event Logos
+c1, c2, c3 = st.columns(3)
+c1.image("./event-img/jito-1.png")
+c2.image("event-img/jpl-logo.png")
+c3.image("event-img/jito-3.png")
+
 # Team Data Import
 team = open("team_names.txt", "r")
 team_names = team.read()
@@ -8,27 +14,39 @@ team_name_list = team_names.split("\n")
 team.close()
 
 # Player Data Import
-player_info = pd.read_csv("player_details.csv")
-player_name = player_info["Player Name"]
-st.subheader("All Players List")
-st.write(player_info)
-st.write('---')
+player_info = pd.read_csv("jito-4-team-list.csv")
+player_name = player_info["Name"]
+#st.subheader("All Players List")
+#st.write(player_info)
+#st.write('---')
 
 index = st.selectbox("Select Player", range(len(player_name)), format_func=lambda x: player_name[x])
 
 # Player data from forms
-player_name = player_info.iat[index, 0]
-player_team = player_info.iat[index, 1]
-player_skills = player_info.iat[index, 2]
+player_name = player_info.iat[index, 1]
+player_age = player_info.iat[index, 3]
+player_specialist = player_info.iat[index, 8]
+player_batsman = player_info.iat[index, 9]
+player_bowler = player_info.iat[index, 10]
+player_played_before = player_info.iat[index, 11]
+player_image = player_info.iat[index, 14]
 
 # Display player info
-co1, co2 = st.columns([1, 2])
-co1.image('sachin.jpg')
+if player_played_before == "Yes":
+    st.success("Player Played Before")
+else:
+    st.error("Not Played Before")
+
+
+co1, co2, co3 = st.columns([1, 2, 1])
+co1.image(player_image)
 co2.header(player_name)
+co3.header(player_age)
 
 col1, col2, col3 = st.columns(3)
-col2.subheader(player_team)
-col3.subheader(player_skills)
+col1.subheader(player_specialist)
+col2.subheader(player_batsman)
+col3.subheader(player_bowler)
 
 # Submit player auction details
 st.form("Player Auction Details", clear_on_submit=True)
@@ -38,3 +56,14 @@ with st.form(key='player_team_form'):
     submitted = st.form_submit_button('Submit')
     if submitted:
         st.balloons()
+
+import requests
+import pandas as pd
+
+image_url = "https://drive.google.com/open?id=1a_MTMISVup-7c22tpmnrRolp4z8TP7RO"
+
+
+r = requests.get(image_url)
+#st.write(r.content)
+#df = pd.read_html(r.text)
+#st.markdown(df)
